9dcaff35e939161785334bb23c06c0a78a7bda93
Lemon <2711665916qq@gmail.com>
2025-10-18 02:57:02 +0800

    更新 thumbnail_view.dart

--- lib/views/reader/widgets/thumbnail_view.dart
+++ lib/views/reader/widgets/thumbnail_view.dart
@@ -1,4 +1,4 @@
-import 'package.flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:haka_comic/network/models.dart';
 
